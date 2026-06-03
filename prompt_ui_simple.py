aac37ae95833a2cad0b00eccf7c10c8fbbeffed8
AMITS-LAPTOP\amits <amit39836@gmail.com>
2026-06-03 17:32:37 +0530

    All done

---
+++ prompt_ui_simple.py
@@ -0,0 +1,19 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "meta-llama/Llama-3.1-8B-Instruct"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+st.header("Research Tool")
+
+user_input = st.text_input('Enter your prompt')
+
+if st.button('Summarize'):
+    result = model.invoke(user_input)
+    st.write(result.content)
